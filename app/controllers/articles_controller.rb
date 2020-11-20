0ee3fb42ca75b2d69b5d2a37edabff5a04b746dd
janis-jenny <pcardenasvm@gmail.com>
2020-11-20 17:46:18 -0500

    added a few gems

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,9 +28,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-
-    flash.notice = "Article '#{@article.title}' deleted!"
-
+    flash.notice = "Article '#{@article.title}' Destroyed!"
     redirect_to articles_path
   end
 
